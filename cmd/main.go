beb60a44a934a328fcb8d8b78ab57ad46357b10f
bNolions <bnolions@h-tech.co>
2021-12-20 18:04:45 +0800

    [fix] 中文亂碼

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"github.com/Nolions/fynePoc/pkg"
 	"github.com/Nolions/fynePoc/view"
+	"os"
 )
 
 func main() {
+	err := os.Setenv("FYNE_FONT", "./assets/wqy-microhei.ttc")
+	if err != nil {
+		return
+	}
 	a := pkg.NewApp()
 	view.MainView(a)
 
 	a.App.Run()
+
+	err = os.Unsetenv("FYNE_FONT")
+	if err != nil {
+		return
+	}
 }
